5fb86991558196f111655783aad9ae97876e52ca
George Washington <owais.ouabdallah@gmail.com>
2025-12-12 14:13:06 -0600

    go

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.querySelector("#RedragonM711Cobra").addEventListener("click", function(){
-    window.location = "http://127.0.0.1:3000/redragonm711cobra.html?serverWindowId=72a4cde7-5f71-4d73-8474-0100e69b0e38";
+    window.location = "./redragonm711cobra.html";
 });
 document.querySelector("#RedragonKumaraK552").addEventListener("click", function(){
-    window.location = "http://127.0.0.1:3000/redragonkumarak552.html?serverWindowId=0fd9fc9c-5bad-43b5-b5bb-5066f70f31af"
+    window.location = "./redragonkumarak552.html"
 })
 function changeRedragonM711CobraToImg1(){
     document.querySelector("#inspectRedragonM711Cobra").src = "redragonM711cobra1.jpg"
