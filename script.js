3430962ce8d9fbc32932dde02a31932eb17a44e9
George Washington <owais.ouabdallah@gmail.com>
2025-12-12 13:54:57 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.querySelector("#RedragonM711Cobra").addEventListener("click", function(){
+    window.location = "http://127.0.0.1:3000/redragonm711cobra.html?serverWindowId=72a4cde7-5f71-4d73-8474-0100e69b0e38";
+});
+document.querySelector("#RedragonKumaraK552").addEventListener("click", function(){
+    window.location = "http://127.0.0.1:3000/redragonkumarak552.html?serverWindowId=0fd9fc9c-5bad-43b5-b5bb-5066f70f31af"
+})
+function changeRedragonM711CobraToImg1(){
+    document.querySelector("#inspectRedragonM711Cobra").src = "redragonM711cobra1.jpg"
+}
+function changeRedragonM711CobraToImg2(){
+    document.querySelector("#inspectRedragonM711Cobra").src = "redragonM711cobra.jpg"
+}
+function changeRedragonM711CobraToImg3(){
+    document.querySelector("#inspectRedragonM711Cobra").src = "redragonM711cobra2.jpg"
+}
+function changeRedragonKumaraK552ToImg1(){
+    document.querySelector("#inspectRedragonKumaraK552").src = "redragonKumaraK552.jpg"
+}
+function changeRedragonKumaraK552ToImg2(){
+    document.querySelector("#inspectRedragonKumaraK552").src = "redragonKumaraK5521.jpg"
+}
+function changeRedragonKumaraK552ToImg3(){
+    document.querySelector("#inspectRedragonKumaraK552").src = "redragonKumaraK5522.jpg"
+}
